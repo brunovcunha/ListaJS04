996520ed07ef1de4fbb9f0217d0229523f912718
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-09-11 20:19:06 -0300

    Update ex11.js

--- ex11/ex11.js
+++ ex11/ex11.js
@@ -5,7 +5,7 @@ balaio = [];
 for (i=1; i<=27; i++) balaio.push(i);
 
 
-for (j=0; j<3;j++) {
+for (j=0; j<4;j++) {
     document.write(`<p>Cartas do Jogador ${j + 1}</p>`);
     for(i=0; i<3;i++){
     indice = Math.round(Math.random()*balaio.length-1);
