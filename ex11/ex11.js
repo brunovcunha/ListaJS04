0b6b1cbbd43fc7ec31489e2e2785c970807aa966
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-09-11 20:17:42 -0300

    Update ex11.js

--- ex11/ex11.js
+++ ex11/ex11.js
@@ -4,31 +4,13 @@ document.write("<h1>Cartas Sorteadas:</h1>");
 balaio = [];
 for (i=1; i<=27; i++) balaio.push(i);
 
-document.write("<p>Cartas do Jogador 1</p>");
-for (i=0; i<3;i++) {
+
+for (j=0; j<3;j++) {
+    document.write(`<p>Cartas do Jogador ${j + 1}</p>`);
+    for(i=0; i<3;i++){
     indice = Math.round(Math.random()*balaio.length-1);
     document.write(`<img src="cartas/carta${balaio[indice]}.png" alt="">`);
     balaio.splice(indice,1);
-}
-
-
-document.write("<p>Cartas do Jogador 2</p>");
-for (i=0; i<3;i++) {
-    indice = Math.round(Math.random()*balaio.length-1);
-    document.write(`<img src="cartas/carta${balaio[indice]}.png" alt="">`);
-    balaio.splice(indice,1);
-}
-
-document.write("<p>Cartas do Jogador 3</p>");
-for (i=0; i<3;i++) {
-    indice = Math.round(Math.random()*balaio.length-1);
-    document.write(`<img src="cartas/carta${balaio[indice]}.png" alt="">`);
-    balaio.splice(indice,1);
-}
-
-document.write("<p>Cartas do Jogador 4</p>");
-for (i=0; i<3;i++) {
-    indice = Math.round(Math.random()*balaio.length-1);
-    document.write(`<img src="cartas/carta${balaio[indice]}.png" alt="">`);
-    balaio.splice(indice,1);
+    }
+    
 }
