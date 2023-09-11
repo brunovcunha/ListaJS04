4df22708d10151c95e7d3cb8e02e6832a632e07b
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-09-11 19:53:50 -0300

    Update ex09.js

--- ex09/ex09.js
+++ ex09/ex09.js
@@ -1,14 +1,15 @@
 document.write(`<h1>Ordem de apresentação do grupo</h1>`)
 
-quant = prompt("Informe a quantidade de participantes do grupo: ");
+quant = prompt("Digite o numero de participantes do grupo: ");
 
-participante = [];
-for (i = 1; i <= quant; i++) {
-    participante[i] = prompt("Digite o nome do " + i + "° participante");
+participantes = [];
+
+for(i = 0; i < quant; i++){
+    participantes.push(prompt("Digite o nome da " + (i + 1) + "° pessoa: "));
 }
 
 for (i = 0; i < quant; i++) {
-    indice = Math.round(Math.random() * (participante.length - 1)) + 1;
-    document.write(`<h3>${i + 1}° ${participante[indice]}</h3>`);
-    participante.splice(indice, 1);
+    indice = Math.round(Math.random() * (participantes.length - 1));
+    document.write(`<h3>${i + 1}° ${participantes[indice]}</h3>`);
+    participantes.splice(indice, 1);
 }
